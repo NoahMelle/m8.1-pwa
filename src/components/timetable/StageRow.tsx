8adee59f219de4c81baa94f3fbae1143b0e4a35d
NoahMelle <noahmkamphuisen@gmail.com>
2025-05-14 22:54:14 +0200

    Add animations to timetable block

--- src/components/timetable/StageRow.tsx
+++ src/components/timetable/StageRow.tsx
@@ -21,7 +21,7 @@ export default function StageRow({
           {acts.map((act) => (
             <button
               key={act.id}
-              className="absolute pr-[8px] block text-white h-full"
+              className="absolute pr-[8px] block text-white h-full  active:pt-1 transition-[padding_0.1s]"
               style={{
                 left: `${getDatePercent(act.startsAt)}%`,
                 width: `${
@@ -36,7 +36,7 @@ export default function StageRow({
               }}
               onClick={() => setHighlightedAct(act)}
             >
-              <span className="bg-red p-2 border-b-4 border-red-900/30 flex flex-col h-full w-full rounded-md text-start text-nowrap truncate">
+              <span className="bg-red p-2 border-b-4 border-red-900/30 transition-[border_0.1s] active:border-0 flex flex-col h-full w-full rounded-md text-start text-nowrap truncate">
                 <span className="truncate">{act.title}</span>
                 <span className="text-xs truncate">
                   {formatDateToTime(act.startsAt)} -{" "}
