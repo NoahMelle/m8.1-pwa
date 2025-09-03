c870e9f10d9466812f3810271c2fd8d9aae48145
NoahMelle <noahmkamphuisen@gmail.com>
2025-09-03 09:38:00 +0200

    feat: add preview page that includes a mockup

---
+++ src/app/(main-layout)/(gradient-bottom-bar)/layout.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function MainLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="grow flex flex-col pb-20">
+      <div className="px-6 py-12 flex flex-col gap-8 grow">{children}</div>
+      <div className="fixed bg-gradient-to-b to-background h-32 bottom-0 w-full left-0 pointer-events-none"></div>
+    </div>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,10 @@ import type { Metadata } from "next";
 import "@/styles/globals.css";
 import "@/styles/app.scss";
 import { cookies } from "next/headers";
-import Navbar from "@/components/Navbar";
-import TopBar from "@/components/global/TopBar";
 import { LanguageProvider } from "@/context/LanguageContext";
 import { getLocaleFromCookies } from "@/i18n/helpers";
 import { ThemeProvider } from "@/context/ThemeContext";
 import { isTheme } from "@/lib/utils";
-import ServiceWorker from "@/components/ServiceWorker";
 
 export const metadata: Metadata = {
   title: "LoveU Festival App",
@@ -32,10 +29,7 @@ export default async function RootLayout({
       <body>
         <ThemeProvider initialTheme={theme}>
           <LanguageProvider initialLanguage={language}>
-            <TopBar />
-            <main className={"grow flex flex-col"}>{children}</main>
-            <Navbar />
-            <ServiceWorker />
+            {children}
           </LanguageProvider>
         </ThemeProvider>
       </body>
